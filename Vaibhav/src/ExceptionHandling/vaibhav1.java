52fae912522dd9df9a57de33200f242345440f33
Vaibhavatnure <vaibhavatnure@gmail.com>
2023-06-08 15:35:30 +0530

    adding vaibhav1 class through vaibhav1 branch

---
+++ Vaibhav/src/ExceptionHandling/vaibhav1.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class vaibhav1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vaibhav1 branch");
+
+	}
+
+}
